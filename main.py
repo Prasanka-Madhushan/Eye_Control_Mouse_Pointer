0c5228c3eacfafbc09fbfe62d48b008396db20f6
Prasanka <prasankamadushan0@gmail.com>
2023-09-17 21:48:41 +0530

    Updated

--- main.py
+++ main.py
@@ -26,5 +26,8 @@ while True:
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
             cv2.circle(frame, (x, y), 3, (0, 255, 255))
+        if (left[0].y - left[1].y) < 0.004:
+            pyautogui.click()
+            pyautogui.sleep(1)
     cv2.imshow('Eye Cntrolled Mouse', frame)
     cv2.waitKey(1)
